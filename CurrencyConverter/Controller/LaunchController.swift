07134d2a4cb17fdfb261cc0699b18eb7d27a2fb5
nickoldschool <nickoldschooll@gmail.com>
2020-07-15 23:33:33 +0300

    New version

---
+++ CurrencyConverter/Controller/LaunchController.swift
@@ -0,0 +1,97 @@
+//
+//  LaunchController.swift
+//  CurrencyConverter
+//
+//  Created by Nick Chekmazov on 15.07.2020.
+//  Copyright © 2020 Nick Chekmazov. All rights reserved.
+//
+
+import UIKit
+
+class LaunchController: UIViewController {
+    
+    let label = UILabel()
+    var pulseLayers = [CAShapeLayer]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.setNavigationBarHidden(true, animated: false )
+        view.backgroundColor = #colorLiteral(red: 0.9892832637, green: 0.6514198184, blue: 0.4338295162, alpha: 1)
+        
+        createPulse()
+        createLabel()
+
+    }
+    
+    private func createLabel() {
+        
+        label.backgroundColor = #colorLiteral(red: 0.3421914876, green: 0.02416796796, blue: 0.4802301526, alpha: 1)
+        label.text = "Currency Converter"
+        label.font = .systemFont(ofSize: 27)
+        //label.layer.borderWidth = 1
+        //label.layer.borderColor = #colorLiteral(red: 0.3421914876, green: 0.02416796796, blue: 0.4802301526, alpha: 1)
+        label.textColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.layer.cornerRadius = 90
+        label.layer.masksToBounds = true
+        view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            label.heightAnchor.constraint(equalToConstant: 180),
+            label.widthAnchor.constraint(equalToConstant: 180),
+        ])
+    }
+    
+
+    private func createPulse() {
+        for _ in 0...2 {
+            let circularPath = UIBezierPath(arcCenter: .zero,
+                                            radius: UIScreen.main.bounds.size.width/2.4,
+                                            startAngle: 0,
+                                            endAngle: 2 * .pi, clockwise: true)
+            let pulseLayer = CAShapeLayer()
+            pulseLayer.path = circularPath.cgPath
+            pulseLayer.lineWidth = 35
+            pulseLayer.fillColor = UIColor.clear.cgColor
+            pulseLayer.lineCap = CAShapeLayerLineCap.round
+            pulseLayer.position = view.center
+            view.layer.addSublayer(pulseLayer)
+            pulseLayers.append(pulseLayer)
+            
+        }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.4) {
+            self.animatePulse(index: 0)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+                self.animatePulse(index: 1)
+//                DispatchQueue.main.asyncAfter(deadline: .now() + 0.8) {
+//                    self.animatePulse(index: 2)
+//                }
+            }
+        }
+    }
+    
+    private func animatePulse(index: Int) {
+        pulseLayers[index].strokeColor = #colorLiteral(red: 0.3421914876, green: 0.02416796796, blue: 0.4802301526, alpha: 1)
+
+        let scaleAnimation = CABasicAnimation(keyPath: "transform.scale")
+        scaleAnimation.duration = 8
+        scaleAnimation.fromValue = 0.0
+        scaleAnimation.toValue = 3.0
+        scaleAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        scaleAnimation.repeatCount = .greatestFiniteMagnitude
+        pulseLayers[index].add(scaleAnimation, forKey: "scale")
+        
+        let opacityAnimation = CABasicAnimation(keyPath: #keyPath(CALayer.opacity))
+        opacityAnimation.duration = 8
+        opacityAnimation.fromValue = 1.0
+        opacityAnimation.toValue = 0.0
+        opacityAnimation.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeOut)
+        opacityAnimation.repeatCount = .greatestFiniteMagnitude
+        pulseLayers[index].add(opacityAnimation, forKey: "opacity")
+    }
+
+}
---
+++ CurrencyConverter/Controller/ViewController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  CurrencyConverter
+//
+//  Created by Nick Chekmazov on 15.07.2020.
+//  Copyright © 2020 Nick Chekmazov. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    // - Outlets
+    let locationLabel = UILabel()
+    //var locationManager: CLLocationManager?
+    
+    // - Constants
+    let locationManager = LocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        view.backgroundColor = .white
+        
+        createLabel()
+        createButton()
+        setCurrentLocation()
+    }
+
+    
+    //MARK: - Create label for location detection
+    
+    private func createLabel() {
+        
+        locationLabel.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        locationLabel.textAlignment = .center
+        locationLabel.numberOfLines = 0
+        view.addSubview(locationLabel)
+        locationLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            locationLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: 150),
+            locationLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            locationLabel.heightAnchor.constraint(equalToConstant: 90),
+            locationLabel.widthAnchor.constraint(equalToConstant: 250),
+        ])
+    }
+    
+    //MARK: - Function for current location inentifying
+    
+    private func setCurrentLocation() {
+        
+        guard let exposedLocation = self.locationManager.exposedLocation else {
+            print("*** Error in \(#function): exposedLocation is nil")
+            return
+        }
+        
+        self.locationManager.getPlace(for: exposedLocation) { placemark in
+            guard let placemark = placemark else { return }
+            
+            var output = "Our location is:"
+            if let country = placemark.country {
+                output = output + "\n\(country)"
+            }
+            if let state = placemark.administrativeArea {
+                output = output + "\n\(state)"
+            }
+            if let town = placemark.locality {
+                output = output + "\n\(town)"
+            }
+            self.locationLabel.text = output
+        }
+    }
+    
+    //MARK: - Create button
+    
+    private func createButton() {
+        
+        let button = UIButton()
+        button.layer.borderWidth = 2
+        button.layer.cornerRadius = 75
+        button.layer.masksToBounds = true
+        button.layer.borderColor =  #colorLiteral(red: 0.9920709729, green: 0.8178209662, blue: 0, alpha: 1)
+        button.setTitleColor( .black, for: .normal )
+        button.setTitle("Next", for: .normal)
+        button.backgroundColor =  #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        button.addTarget(self, action: #selector(tap), for: .touchUpInside)
+        view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 400),
+            button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            button.heightAnchor.constraint(equalToConstant: 150),
+            button.widthAnchor.constraint(equalToConstant: 150),
+        ])
+    }
+    
+    //MARK: - Call button for moving to next view
+    
+    @objc func tap() {
+        
+        let vc = ExchangeTableController()
+        navigationController?.pushViewController(vc,animated: true)
+        
+    }
+
+}
+
