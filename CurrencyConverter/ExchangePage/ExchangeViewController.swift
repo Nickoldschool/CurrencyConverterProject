3d20e05da693def45e99606e0330165441da2c3e
nickoldschool <nickoldschooll@gmail.com>
2020-08-06 17:11:37 +0300

    fix scrollView: made in aspect view

--- CurrencyConverter/ExchangePage/ExchangeViewController.swift
+++ CurrencyConverter/ExchangePage/ExchangeViewController.swift
@@ -32,6 +32,7 @@ final class ExchangeViewController: UIViewController {
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(sender:)),
                                                name: UIResponder.keyboardWillHideNotification, object: nil);
     }
+
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         view.endEditing(true)
@@ -52,6 +53,7 @@ final class ExchangeViewController: UIViewController {
                 let intersectRect = kbFrame.intersection(screenSize)
                 
                 if intersectRect.isNull {
+                    
                     _kbSize = CGSize(width: screenSize.size.width, height: 0)
                 } else {
                     _kbSize = intersectRect.size
@@ -70,9 +72,10 @@ final class ExchangeViewController: UIViewController {
     private func addElements() {
         
         exchangeImage.contentMode = .scaleToFill
-        
-        scrollView.contentSize = CGSize(width: view.bounds.size.width, height: view.bounds.size.height )
-        scrollView.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        scrollView.contentSize = CGSize(width: view.bounds.size.width, height: 950 )
+        purpleView.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        purpleView.layer.cornerRadius = 15
+        //scrollView.backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
         scrollView.layer.cornerRadius = 15
         fromLabel.layer.cornerRadius = 15
         toLabel.layer.cornerRadius = 15
@@ -105,69 +108,80 @@ final class ExchangeViewController: UIViewController {
     
     private func addSubviews() {
         
-        view.addSubview(exchangeImage)
+        //view.addSubview(exchangeImage)
         view.addSubview(scrollView)
-        scrollView.addSubview(fromLabel)
-        scrollView.addSubview(toLabel)
-        scrollView.addSubview(fromTextField)
-        scrollView.addSubview(toTextField)
-        scrollView.addSubview(pushButton)
+        scrollView.addSubview(exchangeImage)
+        scrollView.addSubview(purpleView)
+        purpleView.addSubview(fromLabel)
+        purpleView.addSubview(toLabel)
+        purpleView.addSubview(fromTextField)
+        purpleView.addSubview(toTextField)
+        purpleView.addSubview(pushButton)
     }
     
     private func setupConstraints() {
+                
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+            NSLayoutConstraint.activate([
+                scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+                scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+                scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            ])
         
         exchangeImage.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            exchangeImage.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
-            exchangeImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            exchangeImage.heightAnchor.constraint(equalToConstant: 240),
-            exchangeImage.widthAnchor.constraint(equalToConstant: 350),
-        ])
+          NSLayoutConstraint.activate([
+              exchangeImage.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 50),
+              exchangeImage.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+              exchangeImage.heightAnchor.constraint(equalToConstant: 240),
+              exchangeImage.widthAnchor.constraint(equalToConstant: 350),
+          ])
         
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        NSLayoutConstraint.activate([
-            scrollView.topAnchor.constraint(equalTo: exchangeImage.bottomAnchor, constant: 50),
-            scrollView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            scrollView.widthAnchor.constraint(equalToConstant: view.frame.width - 70),
-            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50),
-        ])
+        purpleView.translatesAutoresizingMaskIntoConstraints = false
+          NSLayoutConstraint.activate([
+              purpleView.topAnchor.constraint(equalTo: exchangeImage.bottomAnchor, constant: 50),
+              purpleView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+              purpleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+              //purpleView.heightAnchor.constraint(equalToConstant: 240),
+              purpleView.widthAnchor.constraint(equalToConstant: 350),
+          ])
         
         fromLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            fromLabel.topAnchor.constraint(equalTo: scrollView.topAnchor, constant: 50),
-            fromLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            fromLabel.topAnchor.constraint(equalTo: purpleView.topAnchor, constant: 30),
+            fromLabel.centerXAnchor.constraint(equalTo: purpleView.centerXAnchor),
             fromLabel.heightAnchor.constraint(equalToConstant: 40),
             fromLabel.widthAnchor.constraint(equalToConstant: 200),
         ])
         
         fromTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            fromTextField.topAnchor.constraint(equalTo: fromLabel.bottomAnchor, constant: 50),
-            fromTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            fromTextField.topAnchor.constraint(equalTo: fromLabel.bottomAnchor, constant: 30),
+            fromTextField.centerXAnchor.constraint(equalTo: purpleView.centerXAnchor),
             fromTextField.heightAnchor.constraint(equalToConstant: 40),
             fromTextField.widthAnchor.constraint(equalToConstant: 200),
         ])
         
         toLabel.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            toLabel.topAnchor.constraint(equalTo: fromTextField.bottomAnchor, constant: 50),
-            toLabel.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            toLabel.topAnchor.constraint(equalTo: fromTextField.bottomAnchor, constant: 30),
+            toLabel.centerXAnchor.constraint(equalTo: purpleView.centerXAnchor),
             toLabel.heightAnchor.constraint(equalToConstant: 40),
             toLabel.widthAnchor.constraint(equalToConstant: 200),
         ])
         
         toTextField.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            toTextField.topAnchor.constraint(equalTo: toLabel.bottomAnchor, constant: 50),
-            toTextField.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            toTextField.topAnchor.constraint(equalTo: toLabel.bottomAnchor, constant: 30),
+            toTextField.centerXAnchor.constraint(equalTo: purpleView.centerXAnchor),
             toTextField.heightAnchor.constraint(equalToConstant: 40),
             toTextField.widthAnchor.constraint(equalToConstant: 200),
         ])
         
         pushButton.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            pushButton.topAnchor.constraint(equalTo: toTextField.bottomAnchor, constant: 50),
-            pushButton.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            pushButton.topAnchor.constraint(equalTo: toTextField.bottomAnchor, constant: 35),
+            pushButton.centerXAnchor.constraint(equalTo: purpleView.centerXAnchor),
             pushButton.heightAnchor.constraint(equalToConstant: 40),
             pushButton.widthAnchor.constraint(equalToConstant: 150),
             pushButton.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: -50),
